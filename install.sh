db86cc06c59a98dc7fdb7c842ebd42cc12b21354
yograf <yograf@gmail.com>
2016-03-09 13:27:42 +0200

    chagne prezto to mine

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ git submodule update --init --recursive
 
 cd $HOME
 sudo rm .bashrc
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/yograf/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 #setopt EXTENDED_GLOB
 #for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 #  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
